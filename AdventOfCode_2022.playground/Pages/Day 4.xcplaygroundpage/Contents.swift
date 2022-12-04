66cd45d7e14ad33e2ea912819a31963ba3bd4fa4
Sam LoBue <flatheadcatfish9@yahoo.com>
2022-12-04 13:42:36 -0800

    Day 4

---
+++ AdventOfCode_2022.playground/Pages/Day 4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+import Foundation
+
+enum BoundCriteria {
+    case fullyContained
+    case overlapped
+}
+
+func findRedundantWorkCount(criteria: BoundCriteria) -> Int {
+    let input = PlaygroundHelper.shared.convertTextFileToString(name: "1-4")!
+    let itemsArray = input.components(separatedBy: "\n")
+    let rangesArray = itemsArray.map { $0.components(separatedBy: ",") }
+    let redundantWorkCount = rangesArray.map { range in
+        let isRedundant = isRedundantWork(criteria: criteria, strings: range)
+        return isRedundant ? 1 : 0
+    }.reduce(0, +)
+    return redundantWorkCount
+}
+
+func isRedundantWork(criteria: BoundCriteria, strings: [String]) -> Bool {
+    let bounds = strings.map { $0.components(separatedBy: "-") }.flatMap { $0 }.compactMap { Int($0) }
+    let rangeOne = bounds[0]...bounds[1]
+    let rangeTwo = bounds[2]...bounds[3]
+    let isFullyContained = (criteria == .fullyContained && (rangeOne.contains(rangeTwo) || rangeTwo.contains(rangeOne)))
+    let isOverlapping = (criteria == .overlapped && (rangeOne.contains(bounds[2]) || rangeTwo.contains(bounds[0])))
+    
+    return isFullyContained || isOverlapping ? true : false
+}
+
+findRedundantWorkCount(criteria: .fullyContained)
+findRedundantWorkCount(criteria: .overlapped)
